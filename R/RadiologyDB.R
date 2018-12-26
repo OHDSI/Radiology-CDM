a79b4ed5a8b8ee34530477bb7cf2bfa003c2afe5
N.K <clax1412@aumc.ac.kr>
2018-12-26 12:59:23 +0900

    Pre-support OMOP CDM v6.0 for BIGINT...

--- R/RadiologyDB.R
+++ R/RadiologyDB.R
@@ -105,7 +105,7 @@ RadDB <- R6::R6Class(classname = "RadDB",
 
         Person_ID <- as.bigint(pID, 4)
         Condition_occurrence_id <- as.integer(coID)
-        Device_concept_id <- as.integer(dcID)
+        Device_concept_id <- as.bigint(dcID, 4)
 
         radiology_modality_concept_ID <- modality  # VARCHAR
         Person_position_concept <- pocID           # VARCHAR
@@ -122,7 +122,7 @@ RadDB <- R6::R6Class(classname = "RadDB",
         Image_exposure_time <- as.float(x = duringTime, digits = 5)
 
         Radiology_dirpath <- rDirPath
-        Visit_occurrence_id <- as.integer(voID)
+        Visit_occurrence_id <- as.bigint(voID, 4)
 
         data.frame(
           radiology_occurrence_ID,
@@ -181,7 +181,7 @@ RadDB <- R6::R6Class(classname = "RadDB",
             if(!dcmRDS$isPixelData())
               next
           }
-          Person_ID[num] <- dcmRDS$getDirectoryID()
+          Person_ID[num] <- as.bigint(dcmRDS$getDirectoryID(), 4)
 
           # PatientPosition is null .... blank
           pocID <- dcmRDS$getOrientation()
