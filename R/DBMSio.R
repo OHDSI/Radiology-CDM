2cf11637d088f63e75e1bb1009c6069f2403e013
N.K <clax1412@aumc.ac.kr>
2019-05-29 17:34:35 +0900

    Fix wrong parameter value..

--- R/DBMSio.R
+++ R/DBMSio.R
@@ -33,12 +33,12 @@ DBMSIO <- R6::R6Class(classname = "DBMSIO",
 
     setlastOccurID = function(tbS, progressBar = F) {
       sql <- readSql(sourceFile = file.path(system.file('extdata/migration', package = 'RadETL', mustWork = T),
-                                            self$dbms, 'getMaxOccurID.sql'))
+                                            private$dbms, 'getMaxOccurID.sql'))
       last_id <- querySql(connection = private$con, sql = sql)
 
       writeLines('Refresh Occurrence ID...')
       sql <- readSql(sourceFile = file.path(system.file('extdata/migration', package = 'RadETL', mustWork = T),
-                                            self$dbms, 'setNewOccurID.sql'))
+                                            private$dbms, 'setNewOccurID.sql'))
       rsql <- render(sql, ohdsiSchema = tbS, cur_id = last_id[1,,] + 1)
       executeSql(connection = private$con, sql = rsql, progressBar = progressBar)
     }
@@ -72,7 +72,7 @@ DBMSIO <- R6::R6Class(classname = "DBMSIO",
         writeLines(text = sprintf('Execute DDL Query for %s', tableName))
         if(createTable) {
           osql <- readSql(sourceFile = file.path(system.file('extdata/ddl', package = 'RadETL', mustWork = T),
-                                                 self$dbms, 'Radiology_Occurrence.sql'))
+                                                 private$dbms, 'Radiology_Occurrence.sql'))
           executeSql(connection = private$con, sql = private$convertSql(osql, ohdsiSchema = tbS))
         }
       } else if(all(colnames(data) == img_cols)) {
@@ -80,7 +80,7 @@ DBMSIO <- R6::R6Class(classname = "DBMSIO",
         writeLines(text = sprintf('Execute DDL Query for %s', tableName))
         if(createTable) {
           osql <- readSql(sourceFile = file.path(system.file('extdata/ddl', package = 'RadETL', mustWork = T),
-                                                 self$dbms, 'Radiology_Image.sql'))
+                                                 private$dbms, 'Radiology_Image.sql'))
           executeSql(connection = private$con, sql = private$convertSql(osql, ohdsiSchema = tbS))
         }
       } else stop("This data is not Radiology CDM \n Please check data and retry...")
@@ -100,7 +100,7 @@ DBMSIO <- R6::R6Class(classname = "DBMSIO",
       ch <- readline("Would you like to continue? [y/N]: ")
       switch(tolower(ch), y = {
         osql <- readSql(sourceFile = file.path(system.file('extdata/ddl', package = 'RadETL', mustWork = T),
-                                               self$dbms, 'rollback', 'Drop_RCDM.sql'))
+                                               private$dbms, 'rollback', 'Drop_RCDM.sql'))
         rsql <- render(sql = osql, ohdsiSchema = tbS)
         writeLines(text = "Drop RCDM tables...")
         executeSql(connection = private$con, sql = translate(rsql, private$dbms), progressBar = T)
